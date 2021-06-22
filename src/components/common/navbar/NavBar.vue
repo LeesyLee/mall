93daca09fc1bf552b881a062acbabb071fa6f487
Leesy-kee <siyaolee@outlook.com>
2021-06-22 09:36:21 +0800

    首页导航栏组件完成

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style>
+  .nav-bar {
+    display: flex;
+    line-height: 44px;
+    height: 44px;
+    text-align: center;
+    box-shadow: 0px 1px 2px rgba(100, 100, 100, .3)
+  }
+  .left, .right {
+    width: 60px;
+    /* background-color: aqua; */
+  }
+  .center {
+    flex: 1;
+    /* background-color: red; */
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
 export default {
-  name: "Home"
-}
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff
+}
 </style>
