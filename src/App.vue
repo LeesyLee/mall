ee7e9fc67a693cada57f547cde459d3c46890114
Leesy-kee <siyaolee@outlook.com>
2021-06-22 09:03:35 +0800

    tabbar引入完成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-
+//导入tabbar
+import MainTabBar from 'components/content/mainTabbar/mainTabBar.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    MainTabBar
+  },
+};
 </script>
 
 <style>
-
+/* @import "./assets/base.css" */
+/* @import "assets/css/base.css" */
+@import "assets/css/base.css";
 </style>
---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="" id="tabbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tabbar {
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #f6f6f6;
+}
+</style>
