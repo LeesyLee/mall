6b719c164810f766a0281aaa4d3674cc9c7311a8
Leesy-kee <siyaolee@outlook.com>
2021-06-24 13:27:54 +0800

    better-scroll(首页平滑滚动效果+返回顶部按钮制作)

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,124 @@
 <template>
-  <div>分类</div>
+  <div class="wrapper">
+    <!-- <button @click="btnClick">点击</button> -->
+    <div>
+      <ul>
+        <li>1我是最胖的</li>
+        <li>2我是最胖的</li>
+        <li>3我是最胖的</li>
+        <li>4我是最胖的</li>
+        <li>5我是最胖的</li>
+        <li>6我是最胖的</li>
+        <li>7我是最胖的</li>
+        <li>8我是最胖的</li>
+        <li>9我是最胖的</li>
+        <li>10我是最胖的</li>
+        <li>11我是最胖的</li>
+        <li>12我是最胖的</li>
+        <li>13我是最胖的</li>
+        <li>14我是最胖的</li>
+        <li>15我是最胖的</li>
+        <li>16我是最胖的</li>
+        <li>17我是最胖的</li>
+        <li>18我是最胖的</li>
+        <li>19我是最胖的</li>
+        <li>20我是最胖的</li>
+        <li>21我是最胖的</li>
+        <li>22我是最胖的</li>
+        <li>23我是最胖的</li>
+        <li>24我是最胖的</li>
+        <li>25我是最胖的</li>
+        <li>26我是最胖的</li>
+        <li>27我是最胖的</li>
+        <li>28我是最胖的</li>
+        <li>29我是最胖的</li>
+        <li>30我是最胖的</li>
+        <li>31我是最胖的</li>
+        <li>32我是最胖的</li>
+        <li>33我是最胖的</li>
+        <li>34我是最胖的</li>
+        <li>35我是最胖的</li>
+        <li>36我是最胖的</li>
+        <li>37我是最胖的</li>
+        <li>38我是最胖的</li>
+        <li>39我是最胖的</li>
+        <li>40我是最胖的</li>
+        <li>41我是最胖的</li>
+        <li>42我是最胖的</li>
+        <li>43我是最胖的</li>
+        <li>44我是最胖的</li>
+        <li>45我是最胖的</li>
+        <li>46我是最胖的</li>
+        <li>47我是最胖的</li>
+        <li>48我是最胖的</li>
+        <li>49我是最胖的</li>
+        <li>50我是最胖的</li>
+        <li>51我是最胖的</li>
+        <li>52我是最胖的</li>
+        <li>53我是最胖的</li>
+        <li>54我是最胖的</li>
+        <li>55我是最胖的</li>
+        <li>56我是最胖的</li>
+        <li>57我是最胖的</li>
+        <li>58我是最胖的</li>
+        <li>59我是最胖的</li>
+        <li>60我是最胖的</li>
+        <li>61我是最胖的</li>
+        <li>62我是最胖的</li>
+        <li>63我是最胖的</li>
+        <li>64我是最胖的</li>
+        <li>65我是最胖的</li>
+        <li>66我是最胖的</li>
+        <li>67我是最胖的</li>
+        <li>68我是最胖的</li>
+      </ul>
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Category'
-}
+
+  import BTscroll from 'better-scroll'
+
+
+  export default {
+    name: 'Category',
+    data() {
+      return {
+        bscroll: null
+      }
+    },
+    mounted() {
+      this.bscroll = new BTscroll(document.querySelector(".wrapper"), {
+        probeType: 3,
+        pullUpLoad: true
+      });
+      this.bscroll.on('scroll', position => {
+        // console.log(position);
+        
+      });
+      this.bscroll.on('pullingUp',() => {
+        console.log('上拉加载更多');
+      })
+    },
+    methods: {
+      btnClick() {
+        console.log(1);
+      },
+
+    }
+  }
+
+
 </script>
 
-<style>
+<style scoped>
+  .wrapper {
+    background-color: pink;
+    height: 180px;
 
+    overflow: hidden;
+    /* overflow-y: scroll; */
+  }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,25 +1,31 @@
 <template>
+
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
-    <home-swiper :banner="banner"></home-swiper>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
 
-    <recommend-view :recommend="recommend"></recommend-view>
+    <!-- 用scroll包裹实现顺滑滚动 -->
+    <scroll class="scroll-content" ref = "Hscroll">
+      <home-swiper :banner="banner"></home-swiper>
+      <recommend-view :recommend="recommend"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control :titles="['流行', '新款', '精选']" class="tab-control" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
 
-    <feature-view></feature-view>
+    <back-top @click.native = "backClick"></back-top>
 
-    <tab-control
-      :titles="['流行', '新款', '精选']"
-      class="tab-control"
-      @tabClick="tabClick"
-    ></tab-control>
+
 
     <!-- <goods-list :goods="goods[this.currentType].list"></goods-list> -->
-    <goods-list :goods="showGoods"></goods-list>
+
+
 
 
     <!-- 暂时放这里占位 -->
-    <ul>
+    <!-- <ul>
       <li>1count</li>
       <li>2count</li>
       <li>3count</li>
@@ -57,115 +63,165 @@
       <li>8count</li>
       <li>9count</li>
       <li>10count</li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
-import HomeSwiper from "./homeComps/HomeSwiper";
-import RecommendView from "./homeComps/RecommendView";
-import FeatureView from "./homeComps/FeatureView";
+  import HomeSwiper from "./homeComps/HomeSwiper";
+  import RecommendView from "./homeComps/RecommendView";
+  import FeatureView from "./homeComps/FeatureView";
 
-import NavBar from "components/common/navbar/NavBar";
-import TabControl from "components/content/tabcontrol/TabControl";
-import GoodsList from "components/content/goods/GoodsList";
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabcontrol/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+  import Scroll from "components/common/scroll/Scroll";
+  import BTscroll from 'better-scroll';
+  import BackTop from 'components/common/backtop/BackTop';
 
-import { getHomeMultidata, getHomeGoods } from "network/home";
+  import { getHomeMultidata, getHomeGoods } from "network/home";
 
-export default {
-  name: "Home",
-  data() {
-    return {
-      banner: [],
-      recommend: [],
-      goods: {
-        pop: { page: 0, list: [] },
-        new: { page: 0, list: [] },
-        sell: { page: 0, list: [] },
-      },
-      currentType: "pop",
-    };
-  },
-  computed: {
-    showGoods(){
-      return this.goods[this.currentType].list;
-    }
-  },
-  components: {
-    NavBar,
-    HomeSwiper,
-    RecommendView,
-    FeatureView,
-    TabControl,
-    GoodsList,
-  },
-  created() {
-    //1.请求多个数据
-    this.mgetHomeMultidata();
-    //2. 请求商品数据
-    this.mgetHomeGoods("pop");
-    this.mgetHomeGoods("new");
-    this.mgetHomeGoods("sell");
-  },
-  methods: {
-    /**
-     * 事件监听相关方法
-     */
-
-    tabClick(index) {
-      switch (index) {
-        case 0:
-          this.currentType = "pop";
-          break;
-        case 1:
-          this.currentType = "new";
-          break;
-        case 2:
-          this.currentType = "sell";
-          break;
+  export default {
+    name: "Home",
+    data() {
+      return {
+        banner: [],
+        recommend: [],
+        goods: {
+          pop: { page: 0, list: [] },
+          new: { page: 0, list: [] },
+          sell: { page: 0, list: [] },
+        },
+        currentType: "pop",
+        bscroll: null
+      };
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list;
       }
     },
-    /**
-     * 网络请求相关方法
-     */
-    //1.请求多个数据
-    mgetHomeMultidata() {
-      getHomeMultidata().then((res) => {
-        this.banner = res.data.banner.list;
-        this.recommend = res.data.recommend.list;
-      });
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList,
+      Scroll,
+      BackTop
     },
-    //2. 请求商品数据
-    mgetHomeGoods(type) {
-      const page = this.goods[type].page + 1;
-      getHomeGoods(type, page).then((res) => {
-        //保存-请求到的所有type类型数据res中
-        console.log(res);
-        this.goods[type].list.push(...res.data.list);
-        this.goods[type].page += 1;
-      });
+    created() {
+      //1.请求多个数据
+      this.mgetHomeMultidata();
+      //2. 请求商品数据
+      this.mgetHomeGoods("pop");
+      this.mgetHomeGoods("new");
+      this.mgetHomeGoods("sell");
     },
-  },
-};
+    mounted() {
+      // console.log(this.$refs.wrapper);
+      // this.bscroll = new BTscroll(this.$refs.wrapper, {
+      // })
+    },
+    methods: {
+      /**
+       * 事件监听相关方法
+       */
+
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = "pop";
+            break;
+          case 1:
+            this.currentType = "new";
+            break;
+          case 2:
+            this.currentType = "sell";
+            break;
+        }
+      },
+
+      backClick() {
+        this.scrollTo(0,0,1000);
+      },
+      //辅助backClick实现点击返回顶部功能
+      scrollTo(x = 0,y = 0,time = 800){
+        //x y time 的默认值分别为 0 0 800
+        // console.log(this.$refs);
+        //选中scroll组件标签
+        console.log(this.$refs.Hscroll);
+        //选中scroll组件中的better-scroll对象
+        console.log(this.$refs.Hscroll.scroll);
+        //利用scroll对象的scrollTo方法实现返回顶部
+        this.$refs.Hscroll.scroll.scrollTo(x,y,time);
+      },
+      
+
+
+
+      /**
+       * 网络请求相关方法
+       */
+      //1.请求多个数据
+      mgetHomeMultidata() {
+        getHomeMultidata().then((res) => {
+          this.banner = res.data.banner.list;
+          this.recommend = res.data.recommend.list;
+        });
+      },
+      //2. 请求商品数据
+      mgetHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then((res) => {
+          //保存-请求到的所有type类型数据res中
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        });
+      },
+    },
+  };
 </script>
 
-<style>
-#home {
-  padding-top: 44px;
-}
-.home-nav {
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  background-color: var(--color-tint);
-  color: #fff;
-  z-index: 3;
-}
+<style scoped>
+  #home {
+    /* padding-top: 44px; */
+    position: relative;
+    height: 100vh;
+  }
 
-.tab-control {
-  position: sticky;
-  top: 44px;
-}
+  .home-nav {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: var(--color-tint);
+    color: #fff;
+    z-index: 3;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
+
+
+  .scroll-content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    overflow: hidden;
+    left: 0;
+    right: 0;
+    /* height: 100%; */
+  }
+
+  /* .scroll-content {
+    margin-top: 44px;
+    height: calc(100% - 44px - 49px);
+    overflow: hidden
+  } */
 </style>
